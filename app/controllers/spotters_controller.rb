271c0be312a3346669646d771a89bf2c1d328af6
aarora16 <a.99.anshul@gmail.com>
2020-11-30 14:15:07 -0600

    spotter controller basic

--- app/controllers/spotters_controller.rb
+++ app/controllers/spotters_controller.rb
@@ -1,3 +1,33 @@
 class SpottersController < ApplicationController
-  
+  def new
+    @spotter = Spotter.new
+  end
+
+  def show
+    @spotter = Spotter.find(params[:id])
+  end
+
+  def edit
+    @spotter = Spotter.find(params[:id])
+  end
+
+  def create
+    @spotter = Spotter.new(spotter_params)
+    
+    if @spotter.save
+      flash[:notice] = "Spotter Created!"
+      redirect_to spotter_path(@espotter)
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+    def spotter_params
+      params.require(:spotter).permit(:first_name, :last_name, :username)
+    end
 end
