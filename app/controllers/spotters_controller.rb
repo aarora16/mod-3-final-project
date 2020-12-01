cc4b647ad00e9b7e7f97d8e15a63b288cfd9a6ae
aarora16 <a.99.anshul@gmail.com>
2020-12-01 09:20:51 -0600

    added functionality for api

--- app/controllers/spotters_controller.rb
+++ app/controllers/spotters_controller.rb
@@ -3,31 +3,45 @@ class SpottersController < ApplicationController
     @spotter = Spotter.new
   end
 
-  def show
-    @spotter = Spotter.find(params[:id])
-  end
-
-  def edit
-    @spotter = Spotter.find(params[:id])
-  end
-
-  def create
-    @spotter = Spotter.new(spotter_params)
-    
-    if @spotter.save
-      flash[:notice] = "Spotter Created!"
-      redirect_to spotter_path(@espotter)
-    else
-      render "new"
+  def index
+    spotters = Spotter.all
+    if spotters
+      render json: spotters, except: [:created_at, :updated_at]
+    else 
+      render json: { message: 'No spotters available'}
     end
   end
 
-  def destroy
-
+  def show
+    spotter = Spotter.find_by(params[:id])
+    if spotter
+      render json: spotter, except: [:created_at, :updated_at]
+    else
+      render json: { message: 'Spotter not found' }
+    end
   end
+
+  # def edit
+  #   @spotter = Spotter.find(params[:id])
+  # end
+
+  # def create
+  #   @spotter = Spotter.new(spotter_params)
+    
+  #   if @spotter.save
+  #     flash[:notice] = "Spotter Created!"
+  #     redirect_to spotter_path(@espotter)
+  #   else
+  #     render "new"
+  #   end
+  # end
+
+  # def destroy
+
+  # end
 
   private
-    def spotter_params
-      params.require(:spotter).permit(:first_name, :last_name, :username)
-    end
+    # def spotter_params
+    #   params.require(:spotter).permit(:first_name, :last_name, :username)
+    # end
 end
